7fb17862235c36f3926c1c7f505593b5c665e2e5
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-14 08:07:38 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 
+// Components
+import Cards from "../Components/Home/Cards";
+
+// Data
+import ImgList from "./../Data/ImgList.json";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      {ImgList ? (
+        <>
+          {ImgList.map((val, key) => {
+            return (
+              <>
+                <Cards key={val.id} />
+              </>
+            );
+          })}
+        </>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
