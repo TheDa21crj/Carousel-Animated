8737aceb85a7ab1d29b094012f46e674a13297eb
Rishav Singh <rishavrajcrj@gmail.com>
2023-08-14 07:45:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
+
+// Loading
+import Loading from "./MicroInterAction/Loading";
 
 export default function App() {
   return (
-    <div className="page">
-      <div className="pageExt">Hello World</div>
-    </div>
+    <BrowserRouter>
+      <div className="page">
+        <div className="pageExt">
+          <Route
+            path="/"
+            element={
+              <Suspense fallback={<Loading />}>
+                <Home />
+              </Suspense>
+            }
+          />
+          Hello World
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
